93ba6a4777c6a838981ef0a4e9c92d6980ee015c
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-06 13:15:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,27 +12,28 @@ public class Main {
   public static void main(String[] args) {
      //create a Scanner
      Scanner input = new Scanner(System.in);
-     //declare a variable for users number
-       int n;
+     //declare a variable 
+       int userNum;
      //declare a variable to count
        int count = 1; 
-       //craete a loop to force the user to type in a positive number 
-       do{
+     //craete a loop to force the user to type in a positive number 
+        do{
+          
           System.out.println("Please enter a positive integer to determine its factors:");
-           n = input.nextInt();
-      }while(n <= 0);
+           userNum = input.nextInt();
+       }while(userNum <= 0);
      
-     //tell user the factors of their number
-      System.out.println("The factors of " + n + " are:");
+      //tell user the factors of their number
+       System.out.println("The factors of " + userNum + " are:");
      
-     //loop stsement for fatctors
-      while(n >= count){
-       if(n % count == 0){
+      //loop stsement for the calculation of factors
+       while(userNum >= count){
+       if(userNum % count == 0){
          System.out.println(count);
       }
          count = count + 1;
       }
-      
+           
   
   }
 }
